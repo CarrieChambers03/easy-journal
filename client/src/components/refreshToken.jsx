664621a2bbaaa5969659d48ab8638a48c0d407cb
Michaela Krčilová <michaela.krcilova@unicornuniversity.net>
2024-05-03 13:46:45 +0200

    fixed refreshToken and refreshTokenAbl

--- client/src/components/refreshToken.jsx
+++ client/src/components/refreshToken.jsx
@@ -8,29 +8,27 @@ export default function RefreshToken(){
     useEffect(() => {
         async function refresh(){
             console.log('called function')
-            if(location.pathname !== '/login' && location.pathname !== '/register'){
-                console.log('passed if function')
-                fetch('http://localhost:8000/user/refresh', {
-                    method: 'GET',
-                    credentials: 'include'
-                })
-                .then(response => {
-                    console.log(response.status)
-                    if(response.status === 401){
-                        navigate('/login');
-                    } else if (response.status === 403){
-                        navigate('/login');
-                    } else {
-                        console.log('Token refreshed');
-                    }
-                })
-                .catch(err => console.log(err));
+            const response = await fetch('http://localhost:8000/user/refresh', {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if(response.status === 401){
+                navigate('/login');
+                return;
+            } else if (response.status === 403){
+                navigate('/login');
+                return;
+            } else {
+                console.log('Token refreshed');
             }
         }
 
         function handleClick() {
             console.log('click');
-            refresh().catch(err => console.log(err));
+            if(location.pathname === '/login' || location.pathname === '/register'){
+                return;
+            }
+            refresh().catch(console.error);
         }
 
         window.addEventListener('click', handleClick);
--- server/abl/user/refreshTokenAbl.js
+++ server/abl/user/refreshTokenAbl.js
@@ -5,14 +5,14 @@ const userDao = require("../../dao/user-dao.js");
 function handleRefreshToken (req, res){
     const cookies = req.cookies;
     if(!cookies?.jwt){
-        return res.sendStatus(401)
+        return res.sendStatus(401).json({ message: "No token"});
     }
 
     const refreshToken = cookies.jwt;
     const userList = userDao.list();
     const user = userList.find(u => u.refreshToken === refreshToken);
     if(!user){
-        return res.status(403)
+        return res.status(403).json({ message: "invalid token"})
     }
 
     jwt.verify(
@@ -20,14 +20,14 @@ function handleRefreshToken (req, res){
         process.env.REFRESH_TOKEN_SECRET,
         (err, decToken) => {
             if(err || user.id !== decToken.id){
-                return res.sendStatus(403)
+                return res.sendStatus(403).json({ message: "no auth"});
             };
             const accessToken = jwt.sign(
                 { id: decToken.id },
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: "5m" }
             );
-            res.status(200).cookie("access", accessToken, { httpOnly: true, secure: true, sameSite: "None", maxAge: 5 * 60 * 1000 });
+            res.status(200).json({message: "okay"}).cookie("access", accessToken, { httpOnly: true, secure: true, sameSite: "None", maxAge: 5 * 60 * 1000 });
         }
     );
 }
